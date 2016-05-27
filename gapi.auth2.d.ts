1c87c7fa159ad709655a80487f320bad82dc0889
Derek Lawless <dlawless@gmail.com>
2016-05-27 15:40:59 -0500

    Fix attachClickHandler onsuccess interface

--- gapi.auth2.d.ts
+++ gapi.auth2.d.ts
@@ -62,7 +62,7 @@ declare module gapi.auth2 {
       fetch_basic_profile?: boolean;
       prompt?: boolean;
       scope?: string;
-    }, onsuccess: () => any, onfailure: (reason: string) => any): any;
+    }, onsuccess: (googleUser: GoogleUser) => any, onfailure: (reason: string) => any): any;
   }
 
   export interface IsSignedIn{
